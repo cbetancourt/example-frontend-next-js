5068f4fe271c4b10e2b78d0532bb89617abda051
Claude Betancourt <cbetancourt@gmail.com>
2020-02-09 21:33:21 -0500

    clean up

--- pages/movies.js
+++ pages/movies.js
@@ -3,14 +3,11 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import sanity from "../lib/sanity";
 import listStyles from "../styles/list";
-import sanityClient from "../lib/sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-const imageBuilder = imageUrlBuilder(sanityClient);
+const imageBuilder = imageUrlBuilder(sanity);
 
-function imageUrlFor(source) {
-  return imageBuilder.image(source);
-}
+const imageUrlFor = source => imageBuilder.image(source);
 
 const query = `*[_type == "movie"] {
   _id,
@@ -22,57 +19,53 @@ const query = `*[_type == "movie"] {
 }[0...50]
 `;
 
-function Movies({ movies }) {
-  return (
-    <Layout>
-      <div className="movies">
-        <ul className="list">
-          {movies.map(movie => (
-            <li key={movie._id} className="list__item">
-              <Link href="/movie/[id]" as={`/movie/${movie._id}`}>
-                <a>
-                  {movie.poster && (
-                    <img
-                      src={imageUrlFor(movie.poster)
-                        .ignoreImageParams()
-                        .width(300)}
-                      width="100"
-                      height={100 / movie.posterAspect}
-                    />
-                  )}
-                  <div style={{ paddingTop: "0.2em" }}>
-                    {movie.releaseDate.substr(0, 4)}
-                  </div>
-                  <h3>{movie.title}</h3>
-                  {movie.director && (
-                    <span className="movies-list__directed-by">
-                      Directed by {movie.director}
-                    </span>
-                  )}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <style jsx>{`
-        .movies {
-          padding: 1rem;
-        }
-        .movies-list__directed-by {
-          display: block;
-          font-size: 1rem;
-        }
-      `}</style>
-      <style jsx>{listStyles}</style>
-    </Layout>
-  );
-}
+const Movies = ({ movies }) => (
+  <Layout>
+    <div className="movies">
+      <ul className="list">
+        {movies.map(movie => (
+          <li key={movie._id} className="list__item">
+            <Link href="/movie/[id]" as={`/movie/${movie._id}`}>
+              <a>
+                {movie.poster && (
+                  <img
+                    src={imageUrlFor(movie.poster)
+                      .ignoreImageParams()
+                      .width(300)}
+                    width="100"
+                    height={100 / movie.posterAspect}
+                  />
+                )}
+                <div style={{ paddingTop: "0.5em" }}>
+                  {movie.releaseDate}
+                </div>
+                <h3>{movie.title}</h3>
+                {movie.director && (
+                  <span className="movies-list__directed-by">
+                    Directed by {movie.director}
+                  </span>
+                )}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <style jsx>{`
+      .movies {
+        padding: 1rem;
+      }
+      .movies-list__directed-by {
+        display: block;
+        font-size: 1rem;
+      }
+    `}</style>
+    <style jsx>{listStyles}</style>
+  </Layout>
+)
 
-Movies.getInitialProps = async () => {
-  return {
-    movies: await sanity.fetch(query)
-  };
-};
+Movies.getInitialProps = async () => ({
+  movies: await sanity.fetch(query)
+});
 
 export default Movies;
